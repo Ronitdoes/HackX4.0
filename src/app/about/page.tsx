6f782d3dd964c1cfd5c169662394cf9337a505de
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-08-02 19:19:21 +0530

    Add History of HackX interactive showcase on About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import WhyHackX from "@/components/WhyHackX";
+import HistoryShowcase from "@/components/HistoryShowcase";
 
 export default function AboutPage() {
   const containerVariants = {
@@ -107,6 +108,9 @@ export default function AboutPage() {
 
       {/* Sticky Content Section */}
       <WhyHackX />
+
+      {/* History & Evolution Showcase */}
+      <HistoryShowcase />
     </div>
   );
 }
